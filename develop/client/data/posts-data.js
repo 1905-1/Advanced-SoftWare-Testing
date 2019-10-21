9eff3548d2ee8bdbc5cde677556c1cee9d3837aa
Unknown <1197063125@qq.com>
2019-10-21 20:15:12 +0800

    applet-update

---
+++ develop/client/data/posts-data.js
@@ -0,0 +1,49 @@
+var local_database = [{
+  date: "July 18 2019",
+  title: '世界上"最长"的步行街---武汉光谷步行街',
+  post_img: "/images/place/guanggu.jpg",
+  author_img: "/images/avatar/1.png",
+  support_img: "/images/icon/support.png",
+  collect_img: "/images/icon/collect.png",
+  content: "光谷步行街 在武汉市洪山区，总建筑面积约150万平方米，现如今已经成为了武汉年轻人最聚集，标志的地方。",
+  support_num: "112",
+  collect_num: "96",
+},
+{
+  date: "Sep 16 2016",
+  title: "世界最长的商业步行街--武汉楚河汉街",
+  post_img: "/images/place/hanjie2.jpg",
+  author_img: "/images/avatar/2.png",
+  support_img: "/images/icon/support.png",
+  collect_img: "/images/icon/collect.png",
+  content: "之前有介绍过中国最高楼的排名，其中介绍到了武汉绿地中心大楼。今天要介绍的是武汉的商业步行街--楚河汉街，它是中国最长的城市商业步行街，也是世界最长的商业步行街。下面就一起看看吧！",
+  support_num: "102",
+  collect_num: "90",
+},
+{
+  date: "Nov 12 2016",
+  title: "天下第一步行街，好吃好玩的江汉路",
+  post_img: "/images/place/jianghanlu2.jpg",
+  author_img: "/images/avatar/3.png",
+  support_img: "/images/icon/support.png",
+  collect_img: "/images/icon/collect.png",
+  content: "都说武汉不逛江汉路总觉得缺点什么，江汉路步行街是中国最长的步行街，有“天下第一步行街”的美誉，在武汉市汉口中心地带。南起沿江大道，贯通中山大道、京汉大道，北至解放大道，全长1600米！！！",
+  support_num: 62,
+  collect_num: 22,
+},
+{
+  date: "Nov 20 2016",
+  title: "引领潮流的先锋---武商广场",
+  content: "武商广场购物中心坐落于武汉市具有黄金商圈美誉的解放大道中段，作为武商”摩尔“商业城的主力百货”，秉持高级精致百货的定位和风貌，1996年开业以来连年蝉联”中国单体百货经济效益第一“，是中国最具影响力的高级百货之一。",
+  post_img: "/images/place/wushangSquare.jpg",
+  author_img: "/images/avatar/4.png",
+  support_img: "/images/icon/support.png",
+  collect_img: "/images/icon/collect.png",
+  support_num: 61,
+  collect_num: 20,
+},
+]
+
+module.exports = {
+  postList: local_database
+}
