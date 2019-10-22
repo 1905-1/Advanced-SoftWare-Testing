cc8c5c46fb7903cae5b286ffa584770ba2c3c01f
dullmancc <13237105778@sina.cn>
2019-10-22 17:11:48 +0800

    个人中心编写
    添加了个人中心模块，用户登录和请求

--- develop/client/app.js
+++ develop/client/app.js
@@ -2,16 +2,36 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
+    var _that = this
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    var token = wx.getStorage({
+      key: 'token',
+      success: function(res) {
+        console.log(res.data)
+        _that.globalData.token = res.data
+        //如果有token直接请求userinfo
+        wx.request({
+          url: _that.globalData.urlPath + 'user/userinfo',
+          data: {
+          },
+          header: {
+            'content-type': 'application/json',
+            'token': _that.globalData.token
+          },
+          method: 'post',
+          success: function (res) {
+            console.log(res.data);
+            _that.globalData.wxAccout = res.data.wxAccout;
+          }
+        })
+      },
+      fail: function (error){
+        console.log("getStorage:"+error.errMsg)
       }
     })
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -21,12 +41,15 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              console.log(res.userInfo)
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
               }
+
+              //如果没有token，用户已经授权登录的情况下，就请求token
+              
             }
           })
         }
@@ -34,6 +57,11 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    urlPath:'http://127.0.0.1:8080/api/wx/',
+    //token要设置请求头中  
+    token:null,
+    //代表后端中用户信息，请求用这个 wxOpenid,或者id
+    wxAccout:null
   }
 })
---
+++ develop/client/pages/personal-center/personal-center.js
@@ -0,0 +1,154 @@
+// pages/user/user.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+      console.log(app.globalData.userInfo);
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    
+  },
+  queryUsreInfo: function () {
+    wx.request({
+      url: app.globalData.urlPath + 'sayHello',
+      data: {
+        openid: app.globalData.openid
+      },
+      header: {
+        'content-type': 'application/json',
+        'token': app.globalData.token
+      },
+      success: function (res) {
+        console.log(res.data);
+        getApp().globalData.userInfo = res.data;
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    if (typeof this.getTabBar === 'function' &&
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 3
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  bindGetUserInfo: function(e) {
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      var that = this;
+      app.globalData.userInfo = e.detail.userInfo
+
+      //插入登录的用户的相关信息到数据库
+      wx.login({
+        success(res) {
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: app.globalData.urlPath + 'user/login',
+              data: {
+                code: res.code,
+                userInfo: app.globalData.userInfo
+              },
+              method:'post',
+              header: {
+                'content-type': 'application/json'
+              },
+              success: function(res) {
+                console.log("插入小程序登录用户信息成功！");
+                console.log(res);
+                //保存token
+                app.globalData.token = res.data.token;
+                wx.setStorageSync("token", res.data.token);
+                that.setData({
+                  hasUserInfo:true,
+                  userInfo: app.globalData.userInfo
+                })
+              },
+              fail: function(error){
+                console.log(error);
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function(res) {
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”')
+          }
+        }
+      })
+    }
+  }
+})
